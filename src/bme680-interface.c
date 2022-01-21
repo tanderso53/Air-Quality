8c1e7674ca533fa6d9a87eac8f436e38afae0380
Tyler Anderson <tasonchou@gmail.com>
2022-01-21 00:04:39 -0600

    Moved BME680 pico interface to separate header and src files

---
+++ src/bme680-interface.c
@@ -0,0 +1,186 @@
+#include "bme680-interface.h"
+
+#include <stdint.h>
+
+#include "pico/stdlib.h"
+
+int8_t bme680_i2c_read(uint8_t reg_addr, uint8_t *reg_data,
+		       uint32_t len, void *intf_ptr)
+{
+	int num_bytes;
+	bme680_intf *intf = (bme680_intf*) intf_ptr;
+
+	if (intf_ptr == NULL) {
+		return 1;
+	}
+
+	/* Write a byte with the register offset first but don't send
+	 * stop. Then begin the read that will start at that offset */
+	i2c_write_blocking(intf->i2c, intf->dev_addr,
+			   &reg_addr, 1, true);
+	num_bytes = i2c_read_blocking(intf->i2c, intf->dev_addr,
+				      reg_data, (uint16_t) len, false);
+
+	if (num_bytes == PICO_ERROR_GENERIC) {
+		return PICO_ERROR_GENERIC;
+	}
+
+	if ((uint32_t) num_bytes != len) {
+		return 1;
+	}
+
+	return 0;
+}
+
+int8_t bme680_i2c_write(uint8_t reg_addr, const uint8_t *reg_data,
+			uint32_t len, void *intf_ptr)
+{
+	int num_bytes;
+	bme680_intf *intf = (bme680_intf*) intf_ptr;
+
+	if (intf_ptr == NULL) {
+		return 1;
+	}
+
+	/* Write a byte with the register offset first but don't send
+	 * stop. Then begin the read that will start at that offset */
+	i2c_write_blocking(intf->i2c, intf->dev_addr,
+			   &reg_addr, 1, true);
+	num_bytes = i2c_write_blocking(intf->i2c, intf->dev_addr,
+				       reg_data, (uint16_t) len, false);
+
+	if (num_bytes == PICO_ERROR_GENERIC) {
+		return PICO_ERROR_GENERIC;
+	}
+
+	if ((uint32_t) num_bytes != len) {
+		return 1;
+	}
+
+	return 0;
+}
+
+void bme680_delay_us(uint32_t period, void *intf_ptr)
+{
+	sleep_us(period);
+}
+
+int init_bme680_sensor(bme680_intf *b_intf, uint8_t dev_addr,
+		       bme680_run_mode mode)
+{
+	uint8_t ret;
+
+	/* needed for logging, hopefully board config picks right
+	 * input/outputs */
+	stdio_init_all();
+	
+	/* Initialize I2C,
+	 * note: given baudrate may note match actual */
+	if (!b_intf->i2c) {
+		b_intf->i2c = i2c_default;
+	}
+
+	i2c_init(b_intf->i2c, 100000);
+
+	/* TODO: Alow gpio of non-default i2c pins to be set up */
+	gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+	gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+	gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+	gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+
+	b_intf->dev_addr = dev_addr;
+
+	/* Set up BME680 */
+	b_intf->bme_dev.intf_ptr = (void*) b_intf;
+	b_intf->bme_dev.intf = BME68X_I2C_INTF;
+	b_intf->bme_dev.amb_temp = 20;
+	b_intf->bme_dev.read = bme680_i2c_read;
+	b_intf->bme_dev.write = bme680_i2c_write;
+	b_intf->bme_dev.delay_us = bme680_delay_us;
+
+	/* Attempt to make initial contact */
+	ret = bme68x_init(&b_intf->bme_dev);
+
+	if (ret != BME68X_OK) {
+		return 1;
+	}
+
+	/* Initial configuration settings and check success */
+	b_intf->conf.filter = BME68X_FILTER_OFF;
+	b_intf->conf.odr = BME68X_ODR_NONE;
+	b_intf->conf.os_hum = BME68X_OS_16X;
+	b_intf->conf.os_pres = BME68X_OS_1X;
+	b_intf->conf.os_temp = BME68X_OS_2X;
+
+	ret = bme68x_set_conf(&b_intf->conf, &b_intf->bme_dev);
+
+	if (ret != BME68X_OK) {
+		return 1;
+	}
+
+	/* Configure heater sequence and check success */
+	b_intf->heatr.enable = BME68X_ENABLE;
+	b_intf->heatr.heatr_temp = 300;
+	b_intf->heatr.heatr_dur = 100;
+	
+	ret = bme68x_set_heatr_conf(BME68X_FORCED_MODE, &b_intf->heatr,
+				    &b_intf->bme_dev);
+
+	if (ret != BME68X_OK) {
+		return 1;
+	}
+
+	return 0;
+}
+
+int sample_bme680_sensor(bme680_run_mode mode, bme680_intf *b_intf,
+			 struct bme68x_data *d)
+{
+	uint8_t ret;
+	uint32_t dur;
+	uint8_t num_fields;
+
+	switch (mode) {
+	case FORCED_MODE:
+		ret = bme68x_set_op_mode(BME68X_FORCED_MODE,
+					 &b_intf->bme_dev);
+
+		if (ret != BME68X_OK) {
+			return 1;
+		}
+
+		dur = bme68x_get_meas_dur(BME68X_FORCED_MODE, &b_intf->conf,
+					  &b_intf->bme_dev)
+			+ (b_intf->heatr.heatr_dur * 1000);
+
+		b_intf->bme_dev.delay_us(dur, b_intf->bme_dev.intf_ptr);
+		ret = bme68x_get_data(BME68X_FORCED_MODE, d, &num_fields,
+				      &b_intf->bme_dev);
+
+		if (ret != BME68X_OK) {
+			return 1;
+		}
+
+		break;
+
+	default:
+		return 1;
+	}
+
+	if (num_fields > 0) {
+		return 0;
+	}
+
+	return 1;
+}
+
+int deinit_bme680_sensor(bme680_intf *b_intf)
+{
+	if (!b_intf->i2c) {
+		return 1;
+	}
+
+	i2c_deinit(b_intf->i2c);
+
+	return 0;
+}
---
+++ src/bme680-interface.h
@@ -0,0 +1,81 @@
+#ifndef BME680_INTERFACE_H
+#define BME680_INTERFACE_H
+
+#include <bme68x.h>
+
+#include "hardware/i2c.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* #ifdef __cplusplus */
+
+	/**
+	 * @brief BME680 interface configuration struct
+	 *
+	 * This is passed to the initialize function to fill out
+	 */
+	typedef struct {
+		i2c_inst_t *i2c;
+		uint8_t dev_addr;
+		struct bme68x_dev bme_dev;
+		struct bme68x_conf conf;
+		struct bme68x_heatr_conf heatr;
+	} bme680_intf;
+
+	/**
+	 * @brief operating modes implemented in the BME68x vendor
+	 * library.
+	 *
+	 * Currently only the forced-mode is implemented in the Pico
+	 * SDK interface.
+	 */
+	typedef enum {
+		FORCED_MODE
+	} bme680_run_mode;
+
+	/**
+	 * @brief Pico-sdk user function to read sensor
+	 */
+	int8_t bme680_i2c_read(uint8_t reg_addr, uint8_t *reg_data,
+			       uint32_t len, void *intf_ptr);
+
+	/**
+	 * @brief Pico-sdk user function to write sensor
+	 */
+	int8_t bme680_i2c_write(uint8_t reg_addr, const uint8_t *reg_data,
+				uint32_t len, void *intf_ptr);
+
+	/**
+	 * @brief Pico-sdk user function for delays by sensor
+	 */
+	void bme680_delay_us(uint32_t period, void *intf_ptr);
+
+	/**
+	 * @brief Initialize and make initial contact with sensor
+	 *
+	 * @param b_intf is the device interface config struct,
+	 * @param dev_addr is the i2c 7-bit device address, and
+	 * @param mode is the starting operating mode. Only forced
+	 * mode is currently implemented.
+	 * Returns 0 if successful and non-zero on error
+	 */
+	int init_bme680_sensor(bme680_intf *b_intf, uint8_t dev_addr,
+			       bme680_run_mode mode);
+
+	/**
+	 * @brief Read measurement from sensor and fill @p d struct
+	 */
+	int sample_bme680_sensor(bme680_run_mode mode, bme680_intf *b_intf,
+				 struct bme68x_data *d);
+
+	/**
+	 * @brief De-initialize pico-sdk's i2c interface
+	 */
+	int deinit_bme680_sensor(bme680_intf *b_intf);
+
+
+#ifdef __cplusplus
+}
+#endif /* #ifdef __cplusplus */
+
+#endif /* #ifndef BME680_INTERFACE_H */
